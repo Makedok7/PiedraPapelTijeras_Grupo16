484e7364b127ca2ee0d1d8b9c580b770a3620b7c
Brian <bjc00285@gmail.com>
2022-09-24 00:36:53 -0300

    pagina de desarrollo version 1.0 sin style .Cordoba Brian

--- src/componentes/Desarrolladores.js
+++ src/componentes/Desarrolladores.js
@@ -1,12 +1,58 @@
-export default function Desarrolladores(){
-    return(
-        <h1>
-            Abalos, Alan Nicolas
-            Cabrera, Fabricio Gaston
-            Cordoba, Juan Brian Joel
-            Macedo, Facundo Emanuel
-            Palacios, Carlos Javier
-            Sosa, Rodrigo Emanuel Lozano
-        </h1>
+import { Route } from "react-router-dom"
+
+export default function Desarrolladores() {
+    const users = [
+        {
+            id: 1,
+            name: 'Abalos, alan Nicoloas',
+            ima: 'https://robohash.org/user1',
+            description: 'hola tengo x años soy estudiante de la unju sueño con hacer videojuegos '
+        },
+        {
+            id: 2,
+            name: 'Cabrera, Fabricio Gaston',
+            ima: 'https://robohash.org/user2',
+            description: 'hola tengo x años soy estudiante de la unju sueño con hacer videojuegos'
+        },
+        {
+            id: 3,
+            name: 'Cordoba, Juan Brian Joel',
+            ima: 'https://robohash.org/user3',
+            description: 'hola tengo x años soy estudiante de la unju sueño con hacer videojuegos '
+        },
+        {
+            id: 4,
+            name: 'Macedo, Facundo Emanuel',
+            ima: 'https://robohash.org/user4',
+            description: 'hola tengo x años soy estudiante de la unju sueño con hacer videojuegos '
+        },
+        {
+            id: 5,
+            name: 'Palacios, Carlos Javier',
+            ima: 'https://robohash.org/user5',
+            description: 'hola tengo x años soy estudiante de la unju sueño con hacer videojuegos '
+        },
+        {
+            id: 6,
+            name: 'Sosa, Rodrigo Emanuel Lozano',
+            ima: 'https://robohash.org/user6',
+            description: 'hola tengo x años soy estudianto de la unju sueño con hacer videojuegos '
+        }
+    ]
+    return (
+        <>
+            <h1>Desarrolladores</h1>
+
+            {users.map((user, i) => {
+                return (<div key={i}>
+                    <h3>{user.name}</h3>
+                    <img src={user.ima} />
+                    <p>{user.description}</p>
+                </div>
+                );
+            }
+            )}
+        </>
     )
+
 }

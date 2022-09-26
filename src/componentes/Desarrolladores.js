45edb5e706268417f6743c03cb2e99742ae9cc2b
Fabricio <reydecopas955@hotmail.com>
2022-09-25 22:00:05 -0300

    Pagina Desarrolladores avance . Cabrera Fabricio

--- src/componentes/Desarrolladores.js
+++ src/componentes/Desarrolladores.js
@@ -8,19 +8,19 @@ export default function Desarrolladores() {
             id: 1,
             name: 'Abalos, Alan Nicolas',
             ima: 'https://robohash.org/user1',
-            description: 'hola tengo x años soy estudiante de la unju sueño con hacer videojuegos '
+            description: 'hola tengo x años soy estudiante de la unju sueño con hacer videojuegos  '
         },
         {
             id: 2,
             name: 'Cabrera, Fabricio Gaston',
             ima: 'https://robohash.org/user2',
-            description: 'hola tengo x años soy estudiante de la unju sueño con hacer videojuegos'
+            description: 'Fan del Anime y los juegos Hack and Slash, vicioso de toda la vida'
         },
         {
             id: 3,
             name: 'Cordoba, Juan Brian Joel',
             ima: 'https://robohash.org/user3',
-            description: 'hola tengo x años soy estudiante de la unju sueño con hacer videojuegos '
+            description: 'Me gusta el asado, hacer musica y jugar a la pelota con amigos los fines de semana'
         },
         {
             id: 4,
@@ -44,15 +44,15 @@ export default function Desarrolladores() {
     return (
         <>
             {Nav()}
-            <h1 className="title my-auto ">Desarrolladores</h1>
+            <h1 className="title my-auto">Desarrolladores</h1>
             <div className="contenedor bg-dark">
                 {users.map((user, i) => {
                     return (<div key={i}>
-                        <section className="list">
+                        <section className="list border  border-5 rounded-start rounded-5">
                             <ul>
-                                <h3 className="name text-center ">{user.name}</h3>
+                                <h3 className="name text-uppercase fst-italic text-center ">{user.name}</h3>
                                 <img className="mx-auto d-block" src={user.ima} />
-                                <p className="description text-center ">{user.description}</p>
+                                <p className="description text-center text-capitalize font-monospace">{user.description}</p>
                             </ul>
                         </section>
                     </div>
